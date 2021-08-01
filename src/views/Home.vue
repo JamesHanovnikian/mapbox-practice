e78a65e85a1719071a1e8358832f8dd7bcc1a0ca
YOUR NAME <jhanov@gmail.com>
2021-08-01 14:57:39 -0500

    practice using markers for different locations using documentation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <button v-on:click="doMapBox()"> Start Mapbox</button>
+    <button v-on:click="locationLoop()"> Show Markers</button>
+     
+    
+    <div id='map' style='width: 400px; height: 300px;'></div>
+    </div> 
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style></style>
 
+<script>
+import mapboxgl from "mapbox-gl";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      places: [
+        { lat: 41.881832, lng: -87.623177, description: "Chicago!" },
+        { lat: 37.9779, lng: 23.77, description: "Zografou!" },
+        { lat: 37.787, lng: 20.8999, description: "zakynthos!" },
+      ],
+    };
+  },
+  mounted: function () {
+    this.doMapBox();
+  },
+  created: function () {},
+  methods: {
+    doMapBox: function () {
+      mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_API_KEY;
+      const map = new mapboxgl.Map({
+        container: "map", // container ID
+        style: "mapbox://styles/mapbox/cjaudgl840gn32rnrepcb9b9g", // style URL
+        center: [-119.5591, 37.715], // starting position [lng, lat]
+        zoom: 9, // starting zoom
+      });
+      map.on("load", function () {
+        map.addSource("dem", {
+          type: "raster-dem",
+          url: "mapbox://mapbox.mapbox-terrain-dem-v1",
+        });
+        map.addLayer(
+          {
+            id: "hillshading",
+            source: "dem",
+            type: "hillshade",
+          },
+          "waterway-river-canal-shadow"
+        );
+      });
+    },
+  },
+};
 </script>
+
